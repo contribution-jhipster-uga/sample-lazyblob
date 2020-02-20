b29e5e70c2ef5181cefa9496f1813a911677b32b
Tom SOLVERY <solverytom@hotmail.fr>
2020-02-20 09:21:43 +0100

    appel de l'api directement dans l'html

--- src/main/webapp/app/entities/photo-photo/photo-photo.component.ts
+++ src/main/webapp/app/entities/photo-photo/photo-photo.component.ts
@@ -129,11 +129,12 @@ export class PhotoPhotoComponent implements OnInit, OnDestroy {
     this.links = this.parseLinks.parse(headers.get('link'));
     this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
     this.photos = data;
-    /* this.photos.forEach(p => {
+    /*this.photos.forEach(p => {
       this.photoService.http
-        .get('/api/photos/' + p.id + '/image', { responseType: 'arraybuffer' })
-        .subscribe(res => (p.image = window.URL.createObjectURL(new Blob([res], { type: 'image/png' }))));
-      this.photoService.http
+        .get('/api/photos/' + p.id + '/image', { responseType: 'text' })
+        .subscribe(res => (p.image = btoa(res)));
+    });
+      /*this.photoService.http
         .get('/api/photos/' + p.id + '/thumbnailx1', { responseType: 'arraybuffer' })
         .subscribe(res => (p.thumbnailx1 = window.URL.createObjectURL(new Blob([res], { type: 'image/png' }))));
       this.photoService.http
